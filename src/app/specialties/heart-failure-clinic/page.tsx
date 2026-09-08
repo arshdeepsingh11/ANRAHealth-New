e111831bf8070d2a4de26626e5d7cd6bf1e88a36
Arshdeep Singh <arshdeeps@bioaro.com>
2026-09-08 00:04:46 -0600

    Add Heart Failure Clinic page

---
+++ src/app/specialties/heart-failure-clinic/page.tsx
@@ -0,0 +1,6 @@
+import SpecialtyPageTemplate from "@/components/SpecialtyPageTemplate";
+import { specialtyContent } from "@/data/specialtyContent";
+
+export default function HeartFailureClinicPage() {
+  return <SpecialtyPageTemplate content={specialtyContent["heart-failure-clinic"]} />;
+}
